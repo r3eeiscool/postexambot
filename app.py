99504508ce68ed52358f5ce18ff20bd6f9632ab8
r3eeiscool <52988200+r3eeiscool@users.noreply.github.com>
2020-10-20 12:11:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,20 @@ while True:
     
     if each_word == "sad":
       feelings_list.append("sad")
-      encouragement_list.append("tomorrow will be better,  feeling sad is being human")
+      encouragement_list.append("it is completely human to be sad, don't be afraid to release your emotions :)")
       counter += 1
     if each_word == "happy":
       feelings_list.append("happy")
-      encouragement_list.append("to keep smiling, live life happily and make every moment your best part")
+      encouragement_list.append("that's great! keep smiling love xx")
       counter += 1
     if each_word == "tired":
       feelings_list.append("tired")
-      encouragement_list.append("you are stronger than you think, dont give up, life is like a marathon and its okay to be slow")
+      encouragement_list.append("life gets a tad too hectic at times, remember to place yourself first and take a breather")
       counter += 1
       
     if each_word == "bored":
       feelings_list.append("bored")
-      encouragement_list.append("Go ocupy yourself, spend time with you loved ones. Life is short, make the most of it.")
+      encouragement_list.append("YOLO, try out something new today!! life's too short to be wasted")
       counter += 1
 
   if counter == 0:
